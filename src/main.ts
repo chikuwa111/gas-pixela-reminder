4f6072d9b9c99288fa1c013b0dd2fbba3d15939f
chikuwa111 <take0shun1111@gmail.com>
2021-05-08 19:16:44 +0900

    Add main feature

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,71 @@
-// Compiled using ts2gas 3.6.4 (TypeScript 4.2.4)
-console.log("Hello World!");
+function main() {
+  const scriptProperties = PropertiesService.getScriptProperties().getProperties();
+  const {
+    PIXELA_USERNAME,
+    PIXELA_GRAPH_ID,
+    PIXELA_TOKEN,
+    SLACK_CHANNEL_ID,
+    SLACK_TOKEN,
+  } = scriptProperties;
+
+  const quantity = getPixelaQuantity({
+    username: PIXELA_USERNAME,
+    graphID: PIXELA_GRAPH_ID,
+    date: new Date(),
+    token: PIXELA_TOKEN,
+  });
+
+  if (quantity > 0) return;
+
+  postSlackMessage({
+    channelId: SLACK_CHANNEL_ID,
+    token: SLACK_TOKEN,
+    text: `${PIXELA_GRAPH_ID}のquantityがまだ0です！`,
+  });
+}
+
+const getPixelaQuantity = ({
+  username,
+  graphID,
+  date,
+  token,
+}: {
+  username: string;
+  graphID: string;
+  date: Date;
+  token: string;
+}): number => {
+  const yyyy = date.getFullYear();
+  const MM = `${date.getMonth() + 1}`.padStart(2, "0");
+  const dd = `${date.getDate()}`.padStart(2, "0");
+  const url = `https://pixe.la/v1/users/${username}/graphs/${graphID}/${yyyy}${MM}${dd}`;
+  const headers = { "X-USER-TOKEN": token };
+
+  const response = UrlFetchApp.fetch(url, { headers });
+  const json = JSON.parse(response.getContentText());
+  return Number(json.quantity);
+};
+
+const postSlackMessage = ({
+  channelId,
+  text,
+  token,
+}: {
+  channelId: string;
+  text: string;
+  token: string;
+}): void => {
+  const url = "https://slack.com/api/chat.postMessage";
+  const formData = {
+    icon_emoji: ":warning:",
+    username: "PixelaBot",
+    channel: channelId,
+    text,
+    token,
+  };
+  const options = {
+    method: "post" as const,
+    payload: formData,
+  };
+  UrlFetchApp.fetch(url, options);
+};
